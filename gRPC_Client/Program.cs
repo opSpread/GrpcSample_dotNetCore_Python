57b53cd41324b1a79c5097e3f04a040921d73364
JhenGuo Chen <bomb399399@gmail.com>
2021-08-25 22:29:13 +0800

    dotNetCoreClient to pythonServer

---
+++ gRPC_Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using Google.Protobuf;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using Grpc.Core;
+
+namespace gRPC_Client
+{
+    class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch(
+           "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress("http://127.0.0.1:50051");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient1" });
+
+
+
+            Console.WriteLine($"Greetings: {reply.Message}");
+            Console.WriteLine("press any key to exit");
+            Console.ReadLine();
+
+
+        }
+    }
+}
